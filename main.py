ef90f354ae5d1a653a1f9b3ccd309e4a9ff7036d
Kinga-Patok <kinga.patok@profil-software.com>
2022-12-12 20:43:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import re
+
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+
+def perform_math():
+    global run
+    global previous
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+    if equation == 'quit':
+        print("See you later")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()"]', ' ', equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    perform_math()
